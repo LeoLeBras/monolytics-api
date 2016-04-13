3b81d1b4ca231e83e94a9d993cb410a4bbb1a31a
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-04-13 13:23:11 +0200

    feat(piratebay): save data

--- app/controllers/PirateBayController.php
+++ app/controllers/PirateBayController.php
@@ -1,6 +1,7 @@
 <?php
 
   require_once(VENDOR_DIR.'/autoload.php');
+  require_once(APP_DIR.'/models/Movie.php');
   use Sunra\PhpSimple\HtmlDomParser;
 
   class PirateBayController {
@@ -18,15 +19,15 @@
     public function get($query) {
 
       // Get data
-      $query = join('+', explode(' ', $query));
-      $dom = HtmlDomParser::file_get_html($this->url.'/s/?q='.$query.'&page=0&orderby=99');
+      $title = join('+', explode(' ', $query));
+      $dom = HtmlDomParser::file_get_html($this->url.'/s/?q='.$title.'&page=0&orderby=99');
       $tbody = $dom->find('#searchResult', 0);
       $text_selector = '_';
       $nodeIndex = 0;
       $seeders = 0;
       $leechers = 0;
       foreach($tbody->find('tr') as $tr) {
-        if($nodeIndex !== 0) {
+        if($nodeIndex !== 0 && $nodeIndex < 6) {
           $torrent_seeders = $tr->find('td', 2)->nodes[0]->$text_selector;
           $torrent_leechers = $tr->find('td', 3)->nodes[0]->$text_selector;
           $seeders += $torrent_seeders[4];
@@ -40,6 +41,15 @@
         'pirate_bay_seeders' => $seeders,
         'pirate_bay_leechers' => $leechers
       );
+
+      // Save $movie in databse
+      $query = new Movie();
+      $query
+        ->where(array(
+          'title' => ucwords(strtolower(join(' ', explode('+', $title))))
+        ))
+        ->set($movie)
+        ->save();
 
       // Show response
       echo json_encode($movie);
