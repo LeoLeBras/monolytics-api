d6c6af4a0c5b86a20179ea4cf0b572b75018374b
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-04-13 21:24:06 +0200

    chore(*): clean routes file

--- app/routes.php
+++ app/routes.php
@@ -8,16 +8,17 @@
 
     // Monolytics API
     '/movies' => 'MoviesController\getAll', // Get all movies
-    '/tops' => 'MoviesController\getTops', // Get all tops movies
-    '/tweets' => 'TwitterController\tops', // Get tweets who referenced #trurank
+    '/tops' => 'MoviesController\getTops',  // Get all tops movies
+    '/tweets' => 'TwitterController\tops',  // Get tweets who referenced #trurank
 
     // APIs
+    '/trakt' => 'TraktController\getTops',
     '/youtube/:movie' => 'YoutubeController\get',
     '/rottentomatoes/:movie' => 'RottenTomatoesController\get',
-    '/trakt' => 'TraktController\getTops',
     '/streaming/:movie' => 'TraktController\getMovie',
     '/omdbapi/:movie' => 'OMDbAPIController\get',
     '/piratebay/:movie' => 'PirateBayController\get',
     '/twitter/:movie' => 'TwitterController\get'
+
 
   );
